14da2ba78c7b65aec631d966d6a10e492a2f13d7
Ильгиз <nice@debian.debian.mi>
2015-12-15 15:45:20 +0300

    Adding *if* to files

--- app/Services/Import.php
+++ app/Services/Import.php
@@ -43,71 +43,77 @@ class Import
             echo 'Файл использований карты найден ' . $cardUses . PHP_EOL;
         }
 
-        //Обработка файла с организациями
-        echo 'Обработка организаций' . PHP_EOL;
-        if (Log::isFileChanged('orgs', $client)) {
-            $array = array();
-            $header = NULL;
-            if (($handle = fopen($client, 'r')) !== FALSE) {
-                while (($row = fgetcsv($handle, 1000, ',')) !== FALSE) {
-                    if (!$header)
-                        $header = $row;
-                    else
-                        $array[] = $row;
+        if ($client)
+        {
+            //Обработка файла с организациями
+            echo 'Обработка организаций' . PHP_EOL;
+            if (Log::isFileChanged('orgs', $client)) {
+                $array = array();
+                $header = NULL;
+                if (($handle = fopen($client, 'r')) !== FALSE) {
+                    while (($row = fgetcsv($handle, 1000, ',')) !== FALSE) {
+                        if (!$header)
+                            $header = $row;
+                        else
+                            $array[] = $row;
+                    }
+                    fclose($handle);
                 }
-                fclose($handle);
+                else {
+                    echo 'Файл не удалось открыть' . PHP_EOL;
+                }
+                $count = 0;
+                foreach ($array as $a) {
+                    $row = User::where('inn', $a[1])->first();
+                    if (!$row)
+                    {
+                        $count += 1;
+                        User::create(['name' => $a[0], 'inn' => $a[1], 'password' => bcrypt($a[2]), 'superUser' => 0]);
+                    }
+                    else
+                    {
+                        $row->update(['password' => bcrypt($a[2])]);
+                        $row->save();
+                    }
+                }
+                echo 'Было добавлено ' . $count . ' записей' . PHP_EOL;
+                unlink($client);
             }
             else {
-                echo 'Файл не удалось открыть' . PHP_EOL;
+                echo 'Новых записей не было найдено' . PHP_EOL;
             }
-            $count = 0;
-            foreach ($array as $a) {
-                $row = User::where('inn', $a[1])->first();
-                if (!$row)
-                {
-                    $count += 1;
-                    User::create(['name' => $a[0], 'inn' => $a[1], 'password' => bcrypt($a[2]), 'superUser' => 0]);
-                }
-                else
-                {
-                    $row->update(['password' => bcrypt($a[2])]);
-                    $row->save();
-                }
-            }
-            echo 'Было добавлено ' . $count . ' записей' . PHP_EOL;
-            unlink($client);
-        }
-        else {
-            echo 'Новых записей не было найдено' . PHP_EOL;
         }
 
-        //Обработка файла использований карты
-        echo 'Обработка использований карты' . PHP_EOL;
-        if (Log::isFileChanged('fuel', $cardUses)) {
-            $array = array();
-            $header = NULL;
-            if (($handle = fopen($cardUses, 'r')) !== FALSE) {
-                while (($row = fgetcsv($handle, 1000, ',')) !== FALSE) {
-                    if (!$header)
-                        $header = $row;
-                    else
-                        $array[] = $row;
+        if ($cardUses)
+        {
+            //Обработка файла использований карты
+            echo 'Обработка использований карты' . PHP_EOL;
+            if (Log::isFileChanged('fuel', $cardUses)) {
+                $array = array();
+                $header = NULL;
+                if (($handle = fopen($cardUses, 'r')) !== FALSE) {
+                    while (($row = fgetcsv($handle, 1000, ',')) !== FALSE) {
+                        if (!$header)
+                            $header = $row;
+                        else
+                            $array[] = $row;
+                    }
+                    fclose($handle);
                 }
-                fclose($handle);
+                else {
+                    echo 'Файл не удалось открыть' . PHP_EOL;
+                }
+                $count = 0;
+                foreach ($array as $a) {
+                    $count += 1;
+                    Fuel::create(['org_id' => $a[0], 'date' => $a[1], 'station' => $a[2], 'value' => $a[3], 'car' => $a[4]]);
+                }
+                echo 'Было добавлено ' . $count . ' записей' . PHP_EOL;
+                unlink($cardUses);
             }
             else {
-                echo 'Файл не удалось открыть' . PHP_EOL;
+                echo 'Новых записей не было найдено' . PHP_EOL;
             }
-            $count = 0;
-            foreach ($array as $a) {
-                $count += 1;
-                Fuel::create(['org_id' => $a[0], 'date' => $a[1], 'station' => $a[2], 'value' => $a[3], 'car' => $a[4]]);
-            }
-            echo 'Было добавлено ' . $count . ' записей' . PHP_EOL;
-            unlink($cardUses);
-        }
-        else {
-            echo 'Новых записей не было найдено' . PHP_EOL;
         }
     }
 }
