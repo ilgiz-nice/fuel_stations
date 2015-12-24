dcaf3eb4dfb9eed07a26df88b1df18de47b9f516
Ильгиз <nice@debian.debian.mi>
2015-12-24 11:52:21 +0300

    Логотип

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -15,12 +15,13 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">{{ Auth::User()->name }}</a>
+                <img src="/images/logo.png" alt="Автоваз" style="height:50px;">
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <p class="navbar-text navbar-right"><a href="/auth/logout/" class="navbar-link">Выйти</a></p>
+                <a class="navbar-brand" href="#" style="float:right;margin-right: 300px;">{{ Auth::User()->name }}</a>
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
     </nav>
